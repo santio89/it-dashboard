0d7d421cd5512b6db54e245d0eaf1082a02e327e
santio89 <santiolais1989@gmail.com>
2024-11-08 23:41:34 -0300

    small-fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@
 export default function Main() {
   return (
     <>
-      <div className="site-section__inner">
+      <div className="site-section__inner home">
         IT DASHBOARD - WORK IN PROGRESS
       </div>
     </>
