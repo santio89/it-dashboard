ccc8b86939ec60d81a1b382cd46534301c79de64
santio89 <santiolais1989@gmail.com>
2024-11-08 23:35:20 -0300

    small-fix

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,10 +2,8 @@
 export default function Admin({ user }) {
   return (
     <>
-      <div className='site-section'>
-        <div className="site-section__inner site-section__list">
-          ADMIN PANEL
-        </div>
+      <div className="site-section__inner site-section__list">
+        ADMIN PANEL
       </div>
     </>
   )
