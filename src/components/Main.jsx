9bfce6696cebfed940cf75381a8a8a19b64de13e
santio89 <santiolais1989@gmail.com>
2024-11-08 22:28:27 -0300

    few-fixes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@
 
 export default function Main() {
   return (
-    <div className='site-section'>
+    <div className='site-section home'>
       <div className="site-section__inner">
         IT DASHBOARD - WORK IN PROGRESS
       </div>
