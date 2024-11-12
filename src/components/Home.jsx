90ef210689e6dfdea8152dda78cfce0f09704b88
santio89 <santiolais1989@gmail.com>
2024-11-12 20:49:21 -0300

    gradient-bg-home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,67 @@
+import { useEffect, useRef, useMemo } from "react";
 import { Link } from "react-router-dom"
+import { Gradient } from "../utils/gradient"
 
 export default function Home() {
+  const gradientRefA = useRef()
+  const gradientRefB = useRef()
+  const gradientRefC = useRef()
+  const gradientRefD = useRef()
+  const gradientA = useMemo(() => new Gradient(), [])
+  const gradientB = useMemo(() => new Gradient(), [])
+  const gradientC = useMemo(() => new Gradient(), [])
+  const gradientD = useMemo(() => new Gradient(), [])
+
+  useEffect(() => {
+    if (gradientRefA.current) {
+      gradientA.initGradient(".canvas-1");
+    }
+  }, [gradientA, gradientRefA]);
+
+  useEffect(() => {
+    if (gradientRefB.current) {
+      gradientB.initGradient(".canvas-2");
+    }
+  }, [gradientB, gradientRefB]);
+
+  useEffect(() => {
+    if (gradientRefC.current) {
+      gradientC.initGradient(".canvas-3");
+    }
+  }, [gradientC, gradientRefC]);
+
+  useEffect(() => {
+    if (gradientRefD.current) {
+      gradientD.initGradient(".canvas-4");
+    }
+  }, [gradientD, gradientRefD]);
+
   return (
     <>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/contacts">
-          <div className="full-bg"></div>
+          <canvas ref={gradientRefA} className="gradient-canvas canvas-1" data-transition-in></canvas>
           <h2>Contacts</h2>
           <p>Manage contacts</p>
         </Link>
       </div>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/devices">
-          <div className="full-bg"></div>
+          <canvas ref={gradientRefB} className="gradient-canvas canvas-2" data-transition-in></canvas>
           <h2>Devices</h2>
           <p>Manage devices</p>
         </Link>
       </div>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/tdl">
-          <div className="full-bg"></div>
+          <canvas ref={gradientRefC} className="gradient-canvas canvas-3" data-transition-in></canvas>
           <h2>Tasks</h2>
           <p>Manage tasks</p>
         </Link>
       </div>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/support">
-          <div className="full-bg"></div>
+          <canvas ref={gradientRefD} className="gradient-canvas canvas-4" data-transition-in></canvas>
           <h2>Support</h2>
           <p>Contact support</p>
         </Link>
