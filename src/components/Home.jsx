4b1c1bd633b5de4ea5f2e4573dac9c7d479dbabd
santio89 <santiolais1989@gmail.com>
2024-11-12 12:30:51 -0300

    fix-home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,29 +3,33 @@ import { Link } from "react-router-dom"
 export default function Home() {
   return (
     <>
-      <div className="site-section__inner home">
-        <div className="site-section__homeCards">
-          <Link className="homeCard" to="/contacts">
-            <div className="full-bg"></div>
-            <h2>Contacts</h2>
-            <p>Manage contacts</p>
-          </Link>
-          <Link className="homeCard" to="/devices">
-            <div className="full-bg"></div>
-            <h2>Devices</h2>
-            <p>Manage devices</p>
-          </Link>
-          <Link className="homeCard" to="/tdl">
-            <div className="full-bg"></div>
-            <h2>Tasks</h2>
-            <p>Manage tasks</p>
-          </Link>
-          <Link className="homeCard" to="/support">
-            <div className="full-bg"></div>
-            <h2>Support</h2>
-            <p>Contact support</p>
-          </Link>
-        </div>
+      <div className="site-section__inner site-section__homeCard" >
+        <Link className="homeCard-anchor" to="/contacts">
+          <div className="full-bg"></div>
+          <h2>Contacts</h2>
+          <p>Manage contacts</p>
+        </Link>
+      </div>
+      <div className="site-section__inner site-section__homeCard" >
+        <Link className="homeCard-anchor" to="/devices">
+          <div className="full-bg"></div>
+          <h2>Devices</h2>
+          <p>Manage devices</p>
+        </Link>
+      </div>
+      <div className="site-section__inner site-section__homeCard" >
+        <Link className="homeCard-anchor" to="/tdl">
+          <div className="full-bg"></div>
+          <h2>Tasks</h2>
+          <p>Manage tasks</p>
+        </Link>
+      </div>
+      <div className="site-section__inner site-section__homeCard" >
+        <Link className="homeCard-anchor" to="/support">
+          <div className="full-bg"></div>
+          <h2>Support</h2>
+          <p>Contact support</p>
+        </Link>
       </div>
     </>
   )
