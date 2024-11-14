e2e01be07f832ac8cf28b7a8152e45ef88c5b4f7
santio89 <santiolais1989@gmail.com>
2024-11-14 10:57:08 -0300

    small fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -49,28 +49,24 @@ export default function Home() {
     <>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/contacts">
-          <div className="bg-gradient"></div>
           <h2>Contacts</h2>
           <p>Manage contacts</p>
         </Link>
       </div>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/devices">
-          <div className="bg-gradient"></div>
           <h2>Devices</h2>
           <p>Manage devices</p>
         </Link>
       </div>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/tdl">
-          <div className="bg-gradient"></div>
           <h2>Tasks</h2>
           <p>Manage tasks</p>
         </Link>
       </div>
       <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/support">
-          <div className="bg-gradient"></div>
           <h2>Support</h2>
           <p>Contact support</p>
         </Link>
