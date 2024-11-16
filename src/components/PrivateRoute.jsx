641d07f6d2089d840760a03a12b76e055b69e4b7
santio89 <santiolais1989@gmail.com>
2024-11-16 01:20:49 -0300

    small fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,16 @@
 import { useSignGoogleMutation } from "../store/slices/apiSlice"
 
 export default function PrivateRoute() {
+    const [signGoogle, resultSignInGoogle] = useSignGoogleMutation()
+
+    const logInGoogle = async () => {
+        await signGoogle()
+    }
+
     return (
         <>
-            <div className="site-section__inner">
-                You must sign in to access this section
+            <div className="site-section__inner site-section__private">
+                You must&nbsp;<button onClick={() => { logInGoogle() }}>sign in</button>&nbsp;to access this section
             </div>
         </>
     )
