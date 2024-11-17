58684693841ddf457d2828e38c83de4bb2a67af8
santio89 <santiolais1989@gmail.com>
2024-11-17 16:24:59 -0300

    small-fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,25 +5,25 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ viewTransitionName: "homeCard-1" }} className="site-section__inner site-section__homeCard" >
+      <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/contacts">
           <h2>Contacts</h2>
           <p>Manage contacts</p>
         </Link>
       </div>
-      <div style={{ viewTransitionName: "homeCard-2" }} className="site-section__inner site-section__homeCard" >
+      <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/devices">
           <h2>Devices</h2>
           <p>Manage devices</p>
         </Link>
       </div>
-      <div style={{ viewTransitionName: "homeCard-3" }} className="site-section__inner site-section__homeCard" >
+      <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/tdl">
           <h2>Tasks</h2>
           <p>Manage tasks</p>
         </Link>
       </div>
-      <div style={{ viewTransitionName: "homeCard-4" }} className="site-section__inner site-section__homeCard" >
+      <div className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/support">
           <h2>Support</h2>
           <p>Contact support</p>
