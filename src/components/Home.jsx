c67521b2bdade991f7da8e73db100942e503f427
santio89 <santiolais1989@gmail.com>
2024-11-17 13:56:35 -0300

    view-transition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,25 +6,25 @@ export default function Home() {
 
   return (
     <>
-      <motion.div layout transition={{ duration: .4 }} className="site-section__inner site-section__homeCard" >
+      <motion.div layout transition={{ duration: .2 }} className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/contacts">
           <h2>Contacts</h2>
           <p>Manage contacts</p>
         </Link>
       </motion.div>
-      <motion.div layout transition={{ duration: .4 }} className="site-section__inner site-section__homeCard" >
+      <motion.div layout transition={{ duration: .2 }} className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/devices">
           <h2>Devices</h2>
           <p>Manage devices</p>
         </Link>
       </motion.div>
-      <motion.div layout transition={{ duration: .4 }} className="site-section__inner site-section__homeCard" >
+      <motion.div layout transition={{ duration: .2 }} className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/tdl">
           <h2>Tasks</h2>
           <p>Manage tasks</p>
         </Link>
       </motion.div>
-      <motion.div layout transition={{ duration: .4 }} className="site-section__inner site-section__homeCard" >
+      <motion.div layout transition={{ duration: .2 }} className="site-section__inner site-section__homeCard" >
         <Link className="homeCard-anchor" to="/support">
           <h2>Support</h2>
           <p>Contact support</p>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,6 +14,17 @@ export default function Main({ section, user }) {
   const sideExpanded = useSelector(state => state.theme.sideExpanded)
   const dispatch = useDispatch()
 
+  const expandSide = () => {
+    try {
+      document.startViewTransition(() => {
+        dispatch(setSideExpanded({ sideExpanded: !sideExpanded }))
+        return
+      })
+    } catch {
+      dispatch(setSideExpanded({ sideExpanded: !sideExpanded }))
+    }
+  }
+
 
   return (
     <div className="mainContainer">
@@ -27,7 +38,7 @@ export default function Main({ section, user }) {
           <NavLink to="/admin">Admin</NavLink>
         </div>
 
-        <button title="Toggle layout" className={`side-toggle ${sideExpanded && "expanded"}`} onClick={() => dispatch(setSideExpanded({ sideExpanded: !sideExpanded }))}>
+        <button title="Toggle layout" className={`side-toggle ${sideExpanded && "expanded"}`} onClick={() => { expandSide() }}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-layout-split" viewBox="0 0 16 16">
             <path d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5-1v12H14a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm-1 0H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h5.5z" />
           </svg>
