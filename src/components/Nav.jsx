002e1bf3daf2b504546a0d819fbd9e08a2934187
santio89 <santiolais1989@gmail.com>
2024-11-20 15:10:47 -0300

    fix-few-styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,6 +31,10 @@ export default function Nav({ rootTheme, user }) {
     rootTheme.current.classList.toggle("light-theme", lightTheme)
   }, [lightTheme])
 
+  useEffect(() => {
+    console.log(user)
+  }, [user])
+
   return (
     <header className="mainHeader">
       <Link className="logo" to="/">IT DASHBOARD</Link>
@@ -51,12 +55,16 @@ export default function Nav({ rootTheme, user }) {
         </div>
         <div className="themeBtn">
           {user ?
-            <button onClick={() => { signOut() }}>
+            <button>
+              <img alt="profile-pic" src={user.photoURL} />
+            </button>
+
+            /*  <button onClick={() => { signOut() }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
                 <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
               </svg>
-            </button>
+            </button>  */
             :
             <button onClick={() => { logInGoogle() }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
