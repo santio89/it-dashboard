8591096ccb4a51f2c045e2daf6d501288ee9edb2
santio89 <santiolais1989@gmail.com>
2024-12-01 13:57:22 -0300

    adding-sort-icons

--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -70,20 +70,30 @@ export default function Devices({ user }) {
         </div>
         {
           isLoadingDevices ? <div className="loader">Loading...</div> :
-            <ul className="items-list" ref={listContainer}>
-              {
-                dataDevices?.map(device => {
-                  if (listSelected === "all" || device.category === listSelected) {
-                    return (<li key={device.id}><button title={device.name} onClick={() => {
-                      dispatch(setModal({ active: true, data: { modalType: "DevicesDataModal", deviceData: true, userId: user?.uid, ...device } })); setListPickerOpen(false)
-                    }}>{device.name}</button></li>)
-                  } else {
-                    return null
+            <div className="listWrapper">
+              <div className="sortBtn">
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sort-alpha-down" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z" />
+                    <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z" />
+                  </svg>
+                </button>
+              </div>
+              <ul className="items-list" ref={listContainer}>
+                {
+                  dataDevices?.map(device => {
+                    if (listSelected === "all" || device.category === listSelected) {
+                      return (<li key={device.id}><button title={device.name} onClick={() => {
+                        dispatch(setModal({ active: true, data: { modalType: "DevicesDataModal", deviceData: true, userId: user?.uid, ...device } })); setListPickerOpen(false)
+                      }}>{device.name}</button></li>)
+                    } else {
+                      return null
+                    }
                   }
+                  )
                 }
-                )
-              }
-            </ul>
+              </ul>
+            </div>
         }
       </div>
       <div className="site-section__inner site-section__chart">
