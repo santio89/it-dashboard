cf0dc3fb6493801f47d37deadd25e91a6187418e
santio89 <santiolais1989@gmail.com>
2024-12-12 01:07:50 -0300

    small-fix

--- src/components/TDL.jsx
+++ src/components/TDL.jsx
@@ -221,6 +221,20 @@ export default function TDL({ user }) {
                 {
                   tdlList?.map((task) =>
                     <li key={task.id} disabled={taskOptions === task.id && (resultEditTdl.isLoading || resultDeleteTdl.isLoading)}>
+                      {/* close btn */}
+                      {taskOptions === task.id &&
+                        <button className='taskOptionsClose' onClick={() => {
+                          if (taskOptions === task.id) {
+                            setTaskOptions(null)
+                          } else {
+                            setTaskOptions(task.id)
+                          }
+                        }}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                          </svg>
+                        </button>
+                      }
                       {/* task options */}
                       {
                         taskOptions === task.id &&
@@ -394,14 +408,11 @@ export default function TDL({ user }) {
                             taskOptions === task.id &&
                             <div className='taskContentTitle'>{task.title}</div>
                           }
-                          <button className={`taskContentBtn ${taskOptions !== task.id && task.priority === "low" && "selectedLow"} ${taskOptions !== task.id && task.priority === "medium" && "selectedMedium"} ${taskOptions !== task.id && task.priority === "high" && "selectedHigh"} ${taskOptions === task.id && "taskOption"}`}
-                            onClick={() => {
-                              if (taskOptions === task.id) {
-                                setTaskOptions(null)
-                              } else {
-                                setTaskOptions(task.id)
-                              }
-                            }}>
+                          <button className={`taskContentBtn ${taskOptions !== task.id && task.priority === "low" && "selectedLow"} ${taskOptions !== task.id && task.priority === "medium" && "selectedMedium"} ${taskOptions !== task.id && task.priority === "high" && "selectedHigh"} ${taskOptions === task.id && "taskOption"}`} onClick={() => {
+                            if (taskOptions !== task.id) {
+                              setTaskOptions(task.id)
+                            }
+                          }} style={{ cursor: taskOptions === task.id ? "default" : "pointer" }}>
                             {taskOptions === task.id ? (task.content || "-") : task.title}
                           </button>
                         </>
@@ -423,7 +434,7 @@ export default function TDL({ user }) {
                         deleteMode === task.id &&
                         <>
                           <div className='taskContentTitle deleteMode'>{task.title}</div>
-                          <button className={`taskContentBtn taskOption deleteMode`} >{task.content || "-"}</button>
+                          <button className={`taskContentBtn taskOption deleteMode`} style={{ cursor: "default" }} >{task.content || "-"}</button>
                         </>
                       }
                     </li>)
