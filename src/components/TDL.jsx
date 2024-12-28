f617bd7b42cdfa8de99abf65af908f89c7a7f605
santio89 <santiolais1989@gmail.com>
2024-12-28 19:51:59 -0300

    small-fix

--- src/components/TDL.jsx
+++ src/components/TDL.jsx
@@ -17,9 +17,6 @@ export default function TDL({ user }) {
   const [listPickerOpen, setListPickerOpen] = useState(false)
   const [listSelected, setListSelected] = useState("all")
 
-  /* edit status */
-  const [editStatus, setEditStatus] = useState(null)
-
   /* edit tdl mutation */
   const [editTdl, resultEditTdl] = useEditTdlMutation()
 
@@ -42,9 +39,7 @@ export default function TDL({ user }) {
       return
     }
 
-    setEditStatus(editStatus => editStatus === "done" ? "not done" : "done")
-
-    const newTask = { ...task, status: editStatus === "done" ? "not done" : "done" }
+    const newTask = { ...task, status: task.status === "done" ? "not done" : "done" }
 
     await editTdl(newTask)
   }
@@ -151,14 +146,13 @@ export default function TDL({ user }) {
                         taskOptions === task.id &&
                         <div className='tdl-btnContainer' onClick={() => {
                           setTaskOptions(null)
-                          setEditStatus(null)
                         }}>
                           {/* task status */}
                           <div className={`tdl-itemData `}>
                             {<>
                               <span>Status:</span>
-                              <button disabled={isFetchingTdl} title={`Status: ${editStatus ?? "not done"}`} onClick={(e) => { e.stopPropagation(); editStatusFn(task) }}>
-                                {editStatus === "done" ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-square" viewBox="0 0 16 16">
+                              <button disabled={isFetchingTdl} title={`Status: ${task.status ?? "not done"}`} onClick={(e) => { e.stopPropagation(); editStatusFn(task) }}>
+                                {task.status === "done" ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-square" viewBox="0 0 16 16">
                                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                                   <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
                                 </svg> :
@@ -197,7 +191,6 @@ export default function TDL({ user }) {
                               <>
                                 <button disabled={isFetchingTdl} title={task.title} className={`taskContentBtn ${taskOptions !== task.id && task.priority === "low" && "selectedLow"} ${taskOptions !== task.id && task.priority === "medium" && "selectedMedium"} ${taskOptions !== task.id && task.priority === "high" && "selectedHigh"} ${taskOptions === task.id && "taskOption"} ${task.status === "done" && "taskDone"}`} onClick={() => {
                                   setTaskOptions(task.id);
-                                  setEditStatus(task.status)
                                 }} >
                                   {task.title}
                                 </button></>
