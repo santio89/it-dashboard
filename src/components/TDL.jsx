b5a40feba5201de1bb371d76954bb7aa0fd708bc
santio89 <santiolais1989@gmail.com>
2024-12-28 01:37:17 -0300

    small-fix

--- src/components/TDL.jsx
+++ src/components/TDL.jsx
@@ -156,6 +156,7 @@ export default function TDL({ user }) {
                           {/* task status */}
                           <div className={`tdl-itemData `}>
                             {<>
+                              Done: 
                               <button disabled={isFetchingTdl} title={`Status: ${editStatus ?? "not done"}`} onClick={(e) => { e.stopPropagation(); editStatusFn(task) }}>
                                 {editStatus === "done" ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-square" viewBox="0 0 16 16">
                                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
