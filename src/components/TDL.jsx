46113d75f32e578fae51ae0f4d33622f95192c6f
santio89 <santiolais1989@gmail.com>
2025-01-01 16:54:44 -0300

    small-fix

--- src/components/TDL.jsx
+++ src/components/TDL.jsx
@@ -168,7 +168,7 @@ export default function TDL({ user }) {
                           <div className={`tdl-optionsBtns`}>
                             {
                               /* open */
-                              <button disabled={isFetchingTdl} title={"Info"} onClick={(e) => { e.stopPropagation(); dispatch(setModal({ active: true, data: { modalType: "TDLDataModal", tdlData: true, ...task, dataList: dataTdl } })); setListPickerOpen(false) }}>
+                              <button title={"Info"} onClick={(e) => { e.stopPropagation(); dispatch(setModal({ active: true, data: { modalType: "TDLDataModal", tdlData: true, ...task, dataList: dataTdl } })); setListPickerOpen(false) }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
                                   <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
                                 </svg>
