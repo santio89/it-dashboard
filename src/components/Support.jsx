90da6e1e3396b62446a4ab9c9fa959097dd02aba
santio89 <santiolais1989@gmail.com>
2025-01-21 16:34:06 -0300

    small-fix

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -135,10 +135,14 @@ export default function Support({ user }) {
                           setTicketOptions(null)
                         }}>
                           {/* ticket status */}
-                          <div className={`tdl-itemData`} title={`Status: ${ticket.status ?? "pending"}`}>
+                          <div className={`tdl-itemData`} title={`Status: ${ticket.status ?? "pending"}`} onClick={(e) => {
+                            e.stopPropagation();
+                            if (ticket.id === "temp-id") return
+                            editStatusFn(ticket)
+                          }}>
                             {<>
                               <span>Status:</span>
-                              <button disabled={ticket.id === "temp-id"} onClick={(e) => { e.stopPropagation(); editStatusFn(ticket) }}>
+                              <button disabled={ticket.id === "temp-id"} >
                                 {ticket.status === "completed" ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-square" viewBox="0 0 16 16">
                                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                                   <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
