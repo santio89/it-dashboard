4fbe049f3d5f58ff09db4bed4f928fb5d14b24ba
santio89 <santiolais1989@gmail.com>
2025-01-24 21:38:35 -0300

    view-transitions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router"
 import SpotlightCard from "./SpotlightCard"
+import { flushSync } from "react-dom"
 
 
 export default function Home() {
@@ -8,25 +9,26 @@ export default function Home() {
     <>
 
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link className="homeCard__link" to="/contacts" viewTransition>
+        {/* forced view transitions */}
+        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/contacts" viewTransition>
           <h2>Contacts</h2>
           <p>Manage contacts</p>
         </Link>
       </SpotlightCard>
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link className="homeCard__link" to="/devices" viewTransition>
+        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/devices" viewTransition>
           <h2>Devices</h2>
           <p>Manage devices</p>
         </Link>
       </SpotlightCard>
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link className="homeCard__link" to="/tasks" viewTransition>
+        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/tasks" viewTransition>
           <h2>Tasks</h2>
           <p>Manage tasks</p>
         </Link>
       </SpotlightCard>
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link className="homeCard__link" to="/support" viewTransition>
+        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/support" viewTransition>
           <h2>Support</h2>
           <p>Contact support</p>
         </Link>
