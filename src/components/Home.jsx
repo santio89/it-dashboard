00c8cba14ce05bc9b9302efaf05408de677507cf
santio89 <santiolais1989@gmail.com>
2025-01-27 10:48:56 -0300

    small-fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,26 +9,25 @@ export default function Home() {
     <>
 
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        {/* forced view transitions */}
-        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/contacts" viewTransition>
+        <Link /* onClick={() => document.startViewTransition(() => flushSync())} */ className="homeCard__link" to="/contacts" viewTransition>
           <h2>Contacts</h2>
           <p>Manage contacts</p>
         </Link>
       </SpotlightCard>
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/devices" viewTransition>
+        <Link /* onClick={() => document.startViewTransition(() => flushSync())} */ className="homeCard__link" to="/devices" viewTransition>
           <h2>Devices</h2>
           <p>Manage devices</p>
         </Link>
       </SpotlightCard>
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/tasks" viewTransition>
+        <Link /* onClick={() => document.startViewTransition(() => flushSync())} */ className="homeCard__link" to="/tasks" viewTransition>
           <h2>Tasks</h2>
           <p>Manage tasks</p>
         </Link>
       </SpotlightCard>
       <SpotlightCard className="homeCard" spotlightColor="var(--main-color-op)">
-        <Link onClick={() => document.startViewTransition(() => flushSync())} className="homeCard__link" to="/support" viewTransition>
+        <Link /* onClick={() => document.startViewTransition(() => flushSync())} */ className="homeCard__link" to="/support" viewTransition>
           <h2>Support</h2>
           <p>Contact support</p>
         </Link>
