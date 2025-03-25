09085fca055f2e12b78b7f4ee0f6933df64e3804
santio89 <santiolais1989@gmail.com>
2025-03-25 11:09:05 -0300

    small-fix

--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -3,5 +3,5 @@ import lang from "../constants/lang";
 
 export const useTranslation = () => {
   const langSelected = useSelector(state => state.theme.lang);
-  return lang[langSelected] || lang.esp;
+  return lang[langSelected] || lang.eng;
 };
