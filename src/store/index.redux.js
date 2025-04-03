f949e347fc96afabe6aeb5fee6af666dc5e706d9
santio89 <santiolais1989@gmail.com>
2025-04-03 19:17:11 -0300

    redux-reconciler

--- src/store/index.redux.js
+++ src/store/index.redux.js
@@ -7,6 +7,7 @@ import apiReducer from "./slices/apiSlice";
 import { apiSlice } from "./slices/apiSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 const rootReducer = combineReducers({
   modal: modalReducer,
@@ -19,6 +20,7 @@ const persistConfig = {
   key: 'root',
   storage,
   whitelist: ["theme", "auth"],
+  stateReconciler: autoMergeLevel2,
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
