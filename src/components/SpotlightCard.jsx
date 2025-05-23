b14407a719e47dd57a59a55a6b4763fc104a2454
santio89 <santiolais1989@gmail.com>
2025-05-23 16:37:37 -0300

    small-fix

--- src/components/SpotlightCard.jsx
+++ src/components/SpotlightCard.jsx
@@ -10,13 +10,19 @@ const SpotlightCard = ({ children, className = "" }) => {
 
     divRef.current.style.setProperty("--mouse-x", `${x}px`);
     divRef.current.style.setProperty("--mouse-y", `${y}px`);
-    divRef.current.style.setProperty("--spotlight-size", `20%`);
   };
+
+  const handleMouseLeave = () => {
+    divRef.current.style.setProperty("--spotlight-size", `0%`);
+    divRef.current.style.setProperty("--mouse-x", ``);
+    divRef.current.style.setProperty("--mouse-y", ``);
+  }
 
   return (
     <div
       ref={divRef}
       onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
       className={`card-spotlight ${className}`}
     >
       {children}
