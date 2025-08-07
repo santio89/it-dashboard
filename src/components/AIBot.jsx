5c41fde577e002f7a4fcfcf2a86d074feb88f76c
santio89 <santiolais1989@gmail.com>
2025-08-07 17:01:35 -0300

    adding-ai-bot

--- src/components/AIBot.jsx
+++ src/components/AIBot.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+import { firebaseAI } from "../config/firebase"
+import { useSelector } from "react-redux";
+
 
 export default function AIBot({ user }) {
+  const langSelected = useSelector(state => state.theme.lang);
+
+  const askBot = async () => {
+
+    const prompt = langSelected === "esp" ? "Escribe una historia corta aleatoria (en español)." : "Write a random short story (in english)."
+
+    const result = await firebaseAI.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    console.log(text);
+  }
+
+
+  useEffect(() => {
+    askBot()
+  }, [])
+
 
   return (
     <>
