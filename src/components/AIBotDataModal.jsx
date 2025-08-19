7f88dff506792dc64d5c8e6ce38f4a8b0873d926
santio89 <santiolais1989@gmail.com>
2025-08-19 11:46:49 -0300

    small-fix

--- src/components/AIBotDataModal.jsx
+++ src/components/AIBotDataModal.jsx
@@ -12,12 +12,13 @@ import rehypeKatex from 'rehype-katex'
 import 'katex/dist/katex.min.css'
 
 
-const copyToClipboard = async (e) => {
+const copyToClipboard = async (e, setCopied) => {
   const text = e.currentTarget.parentNode.querySelector("code").textContent
   try {
     await navigator.clipboard.writeText(text);
+    setCopied(true)
   } catch (e) {
-    console.log('Failed to copy code: ', e);
+    console.log(e);
   }
 };
 
@@ -26,6 +27,18 @@ const components = {
   code(props) {
     const { children, className, node, ...rest } = props
     const match = /language-(\w+)/.exec(className || '')
+    const lang = useTranslation()
+    const [copied, setCopied] = useState(false)
+
+    useEffect(() => {
+      const copyTimeout = copied && setTimeout(() => {
+        setCopied(false)
+      }, 2000)
+
+      return () => {
+        clearTimeout(copyTimeout)
+      }
+    }, [copied])
 
     return match ? <>
       <SyntaxHighlighter
@@ -35,11 +48,20 @@ const components = {
         language={match[1]}
         style={oneDark}
       />
-      <button title="Copy to clipboard" className='copyClipboardBtn' onClick={(e) => copyToClipboard(e)}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard2-fill" viewBox="0 0 16 16">
-          <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z" />
-          <path d="M3.5 1h.585A1.5 1.5 0 0 0 4 1.5V2a1.5 1.5 0 0 0 1.5 1.5h5A1.5 1.5 0 0 0 12 2v-.5q-.001-.264-.085-.5h.585A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1" />
-        </svg>
+      <button title={lang.copyToClipboard} className='copyClipboardBtn' onClick={(e) => copyToClipboard(e, setCopied)}>
+        {
+          copied ?
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+              <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5" />
+              <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708" />
+            </svg>
+            :
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard2-fill" viewBox="0 0 16 16">
+              <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z" />
+              <path d="M3.5 1h.585A1.5 1.5 0 0 0 4 1.5V2a1.5 1.5 0 0 0 1.5 1.5h5A1.5 1.5 0 0 0 12 2v-.5q-.001-.264-.085-.5h.585A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1" />
+            </svg>
+        }
+
       </button>
     </> : <>
       <code {...rest} className={className}>
