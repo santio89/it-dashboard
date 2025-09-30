cc05aaac888e85a641feac94880e0e9749495991
santio89 <santiolais1989@gmail.com>
2025-09-30 12:19:48 -0300

    small-fix

--- src/utils/domainCheck.js
+++ src/utils/domainCheck.js
@@ -1,9 +1,9 @@
 export const domainCheck = (email) => {
   const regex = new RegExp(`@${"orlosh.com.ar".replace('.', '\\.')}$`);
 
-  return (email === "santiolais1989@gmail.com" || regex.test(email));
+  return (email === import.meta.env.VITE_USER_EMAIL || regex.test(email));
 }
 
 export const adminCheck = (email) => {
-  return (email === "santiago.olais@orlosh.com.ar");
+  return (email === import.meta.env.VITE_ADMIN_EMAIL);
 }
